109ab1b3e93ac9ba4d3c41446e908b985ef20084
Zach Andrews <andrewsz@allegheny.edu>
2019-09-05 11:28:56 -0400

    Initial commit

---
+++ evotext.py
@@ -0,0 +1,35 @@
+import string
+import random
+import time
+import sys
+
+possibleCharacters = string.ascii_lowercase + string.digits + string.ascii_uppercase + ' .,!?;:/'
+
+if len(sys.argv) == 2:
+    target = sys.argv[1]
+else:
+    print("Please enter a target word")
+    sys.exit()
+
+attemptThis = ''.join(random.choice(possibleCharacters) for i in range(len(target)))
+attemptNext = ''
+
+completed = False
+
+generation = 0
+
+while completed == False:
+    print(attemptThis)
+    attemptNext = ''
+    completed = True
+    for i in range(len(target)):
+        if attemptThis[i] != target[i]:
+            completed = False
+            attemptNext += random.choice(possibleCharacters)
+        else:
+            attemptNext += target[i]
+    generation += 1
+    attemptThis = attemptNext
+    time.sleep(0.1)
+
+print("Target matched! That took " + str(generation) + " generation(s)")
